d3804917f1c578834610704c6aef51504eb70d16
dengdeng <dengyuanqin@gmail.com>
2018-11-28 15:36:30 +0800

    update

---
+++ src/main/java/com/decathlon/cicd/test/CicdTestApplication.java
@@ -0,0 +1,12 @@
+package com.decathlon.cicd.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CicdTestApplication.class, args);
+  }
+}
---
+++ src/test/java/com/decathlon/cicd/test/CicdTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.decathlon.cicd.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CicdTestApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
